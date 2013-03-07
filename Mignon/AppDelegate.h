f07e93af6c4f7d06762b312fd458570b79a9d5d2
Chang chen hsing <sinss0000@gmail.com>
2013-03-07 21:44:17 +0800

    Initial Commit

---
+++ Mignon/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Mignon
+//
+//  Created by sinss on 13/3/7.
+//  Copyright (c) 2013年 MountainStar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Mignon/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Mignon
+//
+//  Created by sinss on 13/3/7.
+//  Copyright (c) 2013年 MountainStar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
